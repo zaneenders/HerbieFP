989566c81fbf6e4f365608a7dab84028d4dce04d
zane <39070793+zaneenders@users.noreply.github.com>
2024-03-16 14:08:02 -0600

    test case and name space

--- Sources/FPCoreParser/FPCoreLexer.swift
+++ Sources/FPCoreParser/FPCoreLexer.swift
@@ -4,6 +4,10 @@ import RegexBuilder
 https://fpbench.org/spec/fpcore-2.0.html
 */
 
+public enum LexingError: Error {
+    case couldNotConsume(String)
+}
+
 public struct Token {
 
     public let offest: Int
@@ -13,58 +17,56 @@ public struct Token {
         self.offest = offest
         self.type = type
     }
-}
 
-public enum FPCoreToken {
-    case leftParen
-    case fpcore
-    case symbol(String)
-    case string(String)
-    case number(Number)
-    case whiteSpace(String)
-    case colen
-    case rightParen
-}
+    public enum FPCoreToken {
+        case leftParen
+        case fpcore
+        case symbol(String)
+        case string(String)
+        case number(Number)
+        case whiteSpace(String)
+        case colen
+        case rightParen
+    }
 
-public enum Number {
-    case rational(String)
-    case decieml(String)
-}
-
-public enum LexingError: Error {
-    case couldNotConsume(String)
+    public enum Number {
+        case rational(String)
+        case decieml(String)
+    }
 }
 
 public func tokens(_ string: String) throws -> [Token] {
+
     var tokens: [Token] = []
     var index = 0
+
     while index < string.count {
-        let s = next(string, index)
-        if let (t, i) = parseWhiteSpace(s) {
+        let s = Token.next(string, index)
+        if let (t, i) = Token.parseWhiteSpace(s) {
             tokens.append(Token(index, t))
             index += i
-        } else if let (t, i) = parseLeftParen(s) {
+        } else if let (t, i) = Token.parseLeftParen(s) {
             tokens.append(Token(index, t))
             index += i
-        } else if let (t, i) = parseRightParen(s) {
+        } else if let (t, i) = Token.parseRightParen(s) {
             tokens.append(Token(index, t))
             index += i
-        } else if let (t, i) = parseColen(s) {
+        } else if let (t, i) = Token.parseColen(s) {
             tokens.append(Token(index, t))
             index += i
-        } else if let (t, i) = parseFPCore(s) {
+        } else if let (t, i) = Token.parseFPCore(s) {
             tokens.append(Token(index, t))
             index += i
-        } else if let (t, i) = parseString(s) {
+        } else if let (t, i) = Token.parseString(s) {
             tokens.append(Token(index, t))
             index += i
-        } else if let (t, i) = parseRational(s) {
+        } else if let (t, i) = Token.parseRational(s) {
             tokens.append(Token(index, t))
             index += i
-        } else if let (t, i) = parseDecl(s) {
+        } else if let (t, i) = Token.parseDecl(s) {
             tokens.append(Token(index, t))
             index += i
-        } else if let (t, i) = parseSymbol(s) {
+        } else if let (t, i) = Token.parseSymbol(s) {
             tokens.append(Token(index, t))
             index += i
         } else {
@@ -76,87 +78,91 @@ public func tokens(_ string: String) throws -> [Token] {
     return tokens
 }
 
-func parseSymbol(_ str: Substring) -> (FPCoreToken, Int)? {
-    let r = #/[a-zA-Z~!@$%^&*_\-+=<>.?/:][a-zA-Z0-9~!@$%^&*_\-+=<>.?/:]*/#
-    if let match: Regex<Substring>.Match = str.prefixMatch(of: r) {
-        return (
-            FPCoreToken.symbol(String(match.output)),
-            match.output.count
-        )
-    }
-    return nil
-}
+extension Token {
 
-func parseDecl(_ str: Substring) -> (FPCoreToken, Int)? {
-    let r = #/[-+]?([0-9]+(\.[0-9]+)?|\.[0-9]+)(e[-+]?[0-9]+)?/#
-    if let match = str.prefixMatch(of: r) {
-        return (
-            FPCoreToken.number(.decieml((String(match.output.0)))),
-            match.output.0.count
-        )
+    static func parseSymbol(_ str: Substring) -> (FPCoreToken, Int)? {
+        let r = #/[a-zA-Z~!@$%^&*_\-+=<>.?/:][a-zA-Z0-9~!@$%^&*_\-+=<>.?/:]*/#
+        if let match: Regex<Substring>.Match = str.prefixMatch(of: r) {
+            return (
+                FPCoreToken.symbol(String(match.output)),
+                match.output.count
+            )
+        }
+        return nil
     }
-    return nil
-}
 
-func parseRational(_ str: Substring) -> (FPCoreToken, Int)? {
-    let r = #/[+-]?[0-9]+/[0-9]*[1-9][0-9]*/#
-    if let match = str.prefixMatch(of: r) {
-        return (
-            FPCoreToken.number(.rational((String(match.output)))),
-            match.output.count
-        )
+    static func parseDecl(_ str: Substring) -> (FPCoreToken, Int)? {
+        let r = #/[-+]?([0-9]+(\.[0-9]+)?|\.[0-9]+)(e[-+]?[0-9]+)?/#
+        if let match = str.prefixMatch(of: r) {
+            return (
+                FPCoreToken.number(.decieml((String(match.output.0)))),
+                match.output.0.count
+            )
+        }
+        return nil
     }
-    return nil
-}
 
-func parseString(_ str: Substring) -> (FPCoreToken, Int)? {
-    let r = #/"([\x20-\x21\x23-\x5b\x5d-\x7e]|\\["\\])*"/#
-    if let match = str.prefixMatch(of: r) {
-        return (
-            FPCoreToken.string(String(match.output.0)),
-            match.output.0.count
-        )
+    static func parseRational(_ str: Substring) -> (FPCoreToken, Int)? {
+        let r = #/[+-]?[0-9]+/[0-9]*[1-9][0-9]*/#
+        if let match = str.prefixMatch(of: r) {
+            return (
+                FPCoreToken.number(.rational((String(match.output)))),
+                match.output.count
+            )
+        }
+        return nil
     }
-    return nil
-}
 
-func parseColen(_ str: Substring) -> (FPCoreToken, Int)? {
-    if let match: Regex<Substring>.Match = str.prefixMatch(of: ":") {
-        return (FPCoreToken.colen, match.output.count)
+    static func parseString(_ str: Substring) -> (FPCoreToken, Int)? {
+        let r = #/"([\x20-\x21\x23-\x5b\x5d-\x7e]|\\["\\])*"/#
+        if let match = str.prefixMatch(of: r) {
+            return (
+                FPCoreToken.string(String(match.output.0)),
+                match.output.0.count
+            )
+        }
+        return nil
     }
-    return nil
-}
 
-func parseFPCore(_ str: Substring) -> (FPCoreToken, Int)? {
-    if let match: Regex<Substring>.Match = str.prefixMatch(of: "FPCore") {
-        return (FPCoreToken.fpcore, match.output.count)
+    static func parseColen(_ str: Substring) -> (FPCoreToken, Int)? {
+        if let match: Regex<Substring>.Match = str.prefixMatch(of: ":") {
+            return (FPCoreToken.colen, match.output.count)
+        }
+        return nil
     }
-    return nil
-}
 
-func parseLeftParen(_ str: Substring) -> (FPCoreToken, Int)? {
-    if let match: Regex<Substring>.Match = str.prefixMatch(of: "(") {
-        return (FPCoreToken.leftParen, match.output.count)
+    static func parseFPCore(_ str: Substring) -> (FPCoreToken, Int)? {
+        if let match: Regex<Substring>.Match = str.prefixMatch(of: "FPCore") {
+            return (FPCoreToken.fpcore, match.output.count)
+        }
+        return nil
     }
-    return nil
-}
 
-func parseRightParen(_ str: Substring) -> (FPCoreToken, Int)? {
-    if let match: Regex<Substring>.Match = str.prefixMatch(of: ")") {
-        return (FPCoreToken.rightParen, match.output.count)
+    static func parseLeftParen(_ str: Substring) -> (FPCoreToken, Int)? {
+        if let match: Regex<Substring>.Match = str.prefixMatch(of: "(") {
+            return (FPCoreToken.leftParen, match.output.count)
+        }
+        return nil
     }
-    return nil
-}
 
-func parseWhiteSpace(_ str: Substring) -> (FPCoreToken, Int)? {
-    if let match: Regex<Substring>.Match = str.prefixMatch(of: .whitespace) {
-        return (
-            FPCoreToken.whiteSpace(String(match.output)), match.output.count
-        )
+    static func parseRightParen(_ str: Substring) -> (FPCoreToken, Int)? {
+        if let match: Regex<Substring>.Match = str.prefixMatch(of: ")") {
+            return (FPCoreToken.rightParen, match.output.count)
+        }
+        return nil
     }
-    return nil
-}
 
-func next(_ str: String, _ index: Int) -> Substring {
-    str.suffix(from: str.index(str.startIndex, offsetBy: index))
+    static func parseWhiteSpace(_ str: Substring) -> (FPCoreToken, Int)? {
+        if let match: Regex<Substring>.Match = str.prefixMatch(of: .whitespace)
+        {
+            return (
+                FPCoreToken.whiteSpace(String(match.output)), match.output.count
+            )
+        }
+        return nil
+    }
+
+    static func next(_ str: String, _ index: Int) -> Substring {
+        str.suffix(from: str.index(str.startIndex, offsetBy: index))
+    }
 }
--- Tests/FPCoreParserTests/FPCoreParserTests.swift
+++ Tests/FPCoreParserTests/FPCoreParserTests.swift
@@ -1,8 +1,9 @@
-import FPCoreParser
 import XCTest
 
+@testable import FPCoreParser
+
 final class FPCoreParserTests: XCTestCase {
-    func testExample() throws {
+    func testBasic() throws {
         let fpCore: String = """
             (FPCore (x y z)
              :name "fabs fraction 1"
@@ -10,7 +11,41 @@ final class FPCoreParserTests: XCTestCase {
             """
         do {
             let tokens = try tokens(fpCore)
+            let fpCore = parse(tokens)
             XCTAssertEqual(tokens.count, 54)
+            let exp: Expr = .operation(
+                .fabs,
+                [
+                    .operation(
+                        .minus,
+                        [
+                            .operation(
+                                .div,
+                                [
+                                    .operation(
+                                        .plus,
+                                        [
+                                            .symbol(Symbol("x")),
+                                            .number(.decnum("4")),
+                                        ]),
+                                    .symbol(Symbol("y")),
+                                ]),
+                            .operation(
+                                .times,
+                                [
+                                    .operation(
+                                        .div,
+                                        [
+                                            .symbol(Symbol("x")),
+                                            .symbol(Symbol("y")),
+                                        ]),
+                                    .symbol(Symbol("z")),
+                                ]),
+
+                        ])
+                ])
+            let expected = FPCore(exp)
+            XCTAssertEqual(fpCore, expected)
         } catch LexingError.couldNotConsume(let str) {
             XCTFail(str)
         }
