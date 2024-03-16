529a1ec74a4d9ca082768ed89e03ed1a60b6bf85
zane <39070793+zaneenders@users.noreply.github.com>
2024-03-16 14:54:49 -0600

    consuming arguments

--- Sources/FPCoreParser/FPCoreParsing.swift
+++ Sources/FPCoreParser/FPCoreParsing.swift
@@ -1,6 +1,102 @@
 public func parse(_ tokens: [Token]) -> FPCore {
+    parseFPCore(0, tokens)
     let exp: Expr = .symbol(Symbol(""))
     return FPCore(exp)
+}
+
+private func parseFPCore(_ index: Int, _ tokens: [Token]) {
+    var i = consumeWhiteSpace(index, tokens)
+    guard peekLeftParen(i, tokens) && peekFPCore(i + 1, tokens) else {
+        return
+    }
+    i += 2
+    guard let (ci, args) = consumeArguments(i, tokens) else {
+        return
+    }
+    i = ci
+    print(i)
+    print(args)
+    print("here: \(tokens[i])")
+
+}
+
+private func consumeArguments(_ index: Int, _ tokens: [Token])
+    -> (Int, [Argument])?
+{
+    var i = consumeWhiteSpace(index, tokens)
+    guard peekLeftParen(i, tokens) else {
+        return nil
+    }
+    i += 1
+    var consumSymbols = true
+    var args: [Argument] = []
+    while consumSymbols {
+        switch tokens[i].type {
+        case .symbol(let sym):
+            args.append(.symbol(Symbol(sym)))
+            i += 1
+        case .whiteSpace(_):
+            i += 1
+        default:
+            consumSymbols = false
+        }
+    }
+    i = consumeWhiteSpace(i, tokens)
+    guard peekRightParen(i, tokens) else {
+        return nil
+    }
+    return (i + 1, args)
+}
+
+private func peekSymbol(_ index: Int, _ tokens: [Token]) -> Bool {
+    switch tokens[index].type {
+    case .symbol(_):
+        return true
+    default:
+        return false
+    }
+}
+
+private func peekFPCore(_ index: Int, _ tokens: [Token]) -> Bool {
+    switch tokens[index].type {
+    case .fpcore:
+        return true
+    default:
+        return false
+    }
+}
+
+private func peekRightParen(_ index: Int, _ tokens: [Token]) -> Bool {
+    switch tokens[index].type {
+    case .rightParen:
+        return true
+    default:
+        return false
+    }
+}
+
+private func peekLeftParen(_ index: Int, _ tokens: [Token]) -> Bool {
+    switch tokens[index].type {
+    case .leftParen:
+        return true
+    default:
+        return false
+    }
+}
+
+// returns the index of token after last whitespace
+private func consumeWhiteSpace(_ index: Int, _ tokens: [Token]) -> Int {
+    var i = index
+    var consumeWhiteSpace = true
+    while consumeWhiteSpace {
+        switch tokens[i].type {
+        case .whiteSpace(_):
+            i += 1
+        default:
+            consumeWhiteSpace = false
+        }
+    }
+    return i
 }
 
 enum Grammar {
