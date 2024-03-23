48e922d3c30e0f483b3b4c153e3499680340b141
zane <39070793+zaneenders@users.noreply.github.com>
2024-03-23 16:13:26 -0600

    practice manual trnaspose

--- Tests/HerbieTests/HerbieTests.swift
+++ Tests/HerbieTests/HerbieTests.swift
@@ -4,8 +4,39 @@ import XCTest
 
 final class ScribeTests: XCTestCase {
 
-    func testNested() {
-        print("herbie")
+    func testRegimes() {
+        let points = 10
+        let cands = 3
+        let top = Double(200)
+        let min = Double(60)
+        var psums:[[Double]] = []
+        var canSplit = Array(repeatElement(true, count: points))
 
+        for _ in 0..<cands {
+            var errs: [Double]  = []
+            for _ in 0..<points {
+                errs.append(Double.random(in: min...top))
+            }
+            psums.append(errs)
+        }
+        let sums = transpose(psums,canSplit)
+        print(psums)
+        print(sums)
     }
+}
+
+func transpose(_ psums: [[Double]], _ canSplit: [Bool]) -> [Double] {
+    let cands = psums.count
+    let points = canSplit.count
+    guard psums.count > 0 else {
+        return []
+    }
+    var out: [Double] = Array(repeatElement(Double.nan, count: cands * points))
+    let sum = cands * points
+    for i in 0..<sum {
+        let p = i / cands
+        let c = i % cands
+        out[i] = psums[c][p]
+    }
+    return out
 }
